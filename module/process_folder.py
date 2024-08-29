f88c83c5682a86433f87932512329055a9c3ac8b
NANNADA4 <coco96062233@icloud.com>
2024-08-29 11:03:52 +0900

    refactoring

--- module/process_folder.py
+++ module/process_folder.py
@@ -7,42 +7,39 @@
 """
 
 
-from module.compress import *
-
-
 import os
+
+
+from module.compress import extract_bandizip, is_zip_encrypted, error_files
 
 
 def process_folder(folder_path):
     """지정된 폴더를 순회하면서 압축파일 처리"""
     is_compressed_exists = False
-    results = []
     for root, _, files in os.walk(folder_path):
         compress_ext = {'.zip', '.egg', '.7z', '.alz'}
         # .vol2.egg ~ .vol50.egg
         exclude_patterns = {f'.vol{i}.egg' for i in range(2, 51)}
-        compress_files = [f for f in files if f.lower().endswith(tuple(compress_ext)) and not any(
+        compress_file_path = [os.path.join('\\\\?\\', root, f) for f in files if f.lower().endswith(tuple(compress_ext)) and not any(
             f.lower().endswith(pattern) for pattern in exclude_patterns)]
-        exclude_files = [f for f in files if f.lower().endswith(
+        exclude_files_path = [os.path.join('\\\\?\\', root, f) for f in files if f.lower().endswith(
             tuple(exclude_patterns))]
 
-        for compress_file in compress_files:
-            compress_file_path = os.path.join('\\\\?\\', root, compress_file)
-            if compress_file_path in error_files:
+        for compress_file in compress_file_path:
+            if compress_file in error_files:
                 continue  # 에러가 발생한 파일 건너뛰기
-            if compress_file_path.lower().endswith('.zip'):
-                if is_zip_encrypted(compress_file_path):
+            if compress_file.lower().endswith('.zip'):
+                if is_zip_encrypted(compress_file):
                     continue  # 암호화된 압축 파일 건너뛰기
-            file_list = extract_bandizip(compress_file_path)
+            file_list = extract_bandizip(compress_file)
             for file in file_list:
                 if file.lower().endswith(tuple(compress_ext)):
-                    if is_compressed_exists == False:
+                    if is_compressed_exists is False:
                         print("내부 압축파일 발견. 스크립트가 한번 더 진행됩니다.")
                         is_compressed_exists = True
 
-    if exclude_files:
-        for rm_file in exclude_files:
-            rm_file_path = os.path.join('\\\\?\\', root, rm_file)
-            os.remove(rm_file_path)
+    if exclude_files_path:
+        for rm_file in exclude_files_path:
+            os.remove(rm_file)
 
     return is_compressed_exists
