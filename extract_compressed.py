6d2a95fa48cb2d15dd7d415d0ea1660040e635b4
NANNADA4 <coco96062233@icloud.com>
2024-09-19 15:34:34 +0900

    improved main fun

--- extract_compressed.py
+++ extract_compressed.py
@@ -10,14 +10,31 @@ import os
 from module.process_folder import process_folder
 
 
-if __name__ == "__main__":
-    folder_path = input("폴더 경로를 입력하세요 : ")
+def main():
+    """main 함수. 내부 압축파일이 존재하지 않을 때까지 반복"""
+    print("\n>>>>>>압축파일 해제<<<<<<\n")
+    print("-"*24)
+    folder_path = input("폴더 경로를 입력하세요 (종료는 0을 입력) : ")
+
+    if folder_path == '0':
+        return 0
+
+    if not os.path.isdir(folder_path):
+        print("입력 폴더의 경로를 다시 한번 확인하세요")
+        return main()
+
     folder_path = os.path.join("\\\\?\\", folder_path)
-    CNT = 1
-    print(f"압축해제 {CNT}번째 진행 중")
-    IS_COMPRESS_EXISTS = process_folder(folder_path)
-    while IS_COMPRESS_EXISTS:
-        CNT += 1
-        print(f"압축해제 {CNT}번째 진행 중")
-        IS_COMPRESS_EXISTS = process_folder(folder_path)
-    print("모든 압축파일 해제가 완료되었습니다")
+    cnt = 1
+    print(f"압축해제 {cnt}번째 진행 중")
+    is_compressed_exists = process_folder(folder_path)
+    while is_compressed_exists:
+        cnt += 1
+        print(f"압축해제 {cnt}번째 진행 중")
+        is_compressed_exists = process_folder(folder_path)
+    print("\n~~~모든 압축파일 해제가 완료되었습니다~~~")
+
+    return main()
+
+
+if __name__ == "__main__":
+    main()
