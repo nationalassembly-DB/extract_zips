e0bd5754d12a9c80e86ee1f94ae2a977fe120eac
NANNADA4 <coco96062233@icloud.com>
2024-10-10 15:35:02 +0900

    -

--- extract_compressed.py
+++ extract_compressed.py
@@ -12,6 +12,7 @@ from module.process_folder import process_folder
 
 def main():
     """main 함수. 내부 압축파일이 존재하지 않을 때까지 반복"""
+    print("-"*24)
     print("\n>>>>>>압축파일 해제<<<<<<\n")
     print("-"*24)
     folder_path = input("폴더 경로를 입력하세요 (종료는 0을 입력) : ")
