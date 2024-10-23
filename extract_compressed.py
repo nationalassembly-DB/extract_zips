93c21e251035ef469fd0468729f4de84896861c1
NANNADA4 <coco96062233@icloud.com>
2024-10-23 11:07:48 +0900

    refactoring

--- extract_compressed.py
+++ extract_compressed.py
@@ -7,7 +7,8 @@ main함수.
 import os
 
 
-from module.log import get_log_path
+from module.describe_script import describe
+from module.log import remove_log
 from module.process_folder import process_folder
 
 
@@ -23,21 +24,11 @@ def main():
         return 0
 
     if select_input == '5':
-        print("- 최초 압축해제시도 : 처음으로 압축해제할 때 사용할 수 있습니다.")
-        print("- 해제되지 않은 압축파일 재시도 : 파일의 갯수가 너무 많아질 경우 압축파일 내 압축파일을 인식하지 못하는 경우가 발생합니다.")
-        print("이럴경우, 이를 사용해 최상위 압축파일명 정보를 훼손하지 않고 기존 log 데이터를 사용해 압축해제를 재시도 할 수 있습니다.")
-        print("- 로그 파일은 프로그램이 위치하는 디렉토리에 생성이 됩니다. 오류가 발생할 경우, 쓰기 권한을 확인하거나 프로그램 위치를 옮기세요")
-        print("로그 파일은 최상위 압축파일명 정보를 입력하기 위해 꼭 필요합니다.")
-        print("압축 해제가 \"완벽하게\" 되었다는 전제 하에 삭제하시기 바랍니다. 로그 파일이 없으면 최상위 압축파일명 데이터는 훼손됩니다")
-
+        describe()
         return main()
 
     if select_input == '9':
-        if os.path.exists(get_log_path()):
-            os.remove(get_log_path())
-            print("로그 파일이 정상적으로 삭제되었습니다")
-        else:
-            print("로그 파일이 존재하지 않습니다.")
+        remove_log()
         return main()
 
     folder_path = input(
@@ -57,7 +48,6 @@ def main():
         process_folder(folder_path, excel_path, 90)
     else:
         print('잘못 입력하셨습니다. 다시 시도하세요')
-
         return main()
 
     print("\n~~~모든 압축파일 해제가 완료되었습니다~~~\n")
--- module/log.py
+++ module/log.py
@@ -11,3 +11,12 @@ def get_log_path():
         return f'{tmp_path}\\Desktop\\log_zips\\zip_file.txt'  # pylint: disable=W0212
     os.makedirs('./log', exist_ok=True)
     return './log/zip_file.txt'
+
+
+def remove_log():
+    """로그 파일을 삭제합니다"""
+    if os.path.exists(get_log_path()):
+        os.remove(get_log_path())
+        print("로그 파일이 정상적으로 삭제되었습니다")
+    else:
+        print("로그 파일이 존재하지 않습니다.")
